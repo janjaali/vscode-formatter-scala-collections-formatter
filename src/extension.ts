ab790c15236680386e1d624e0b3da149ebe74e60
Siyavash Habashi <ghashange@live.com>
2020-09-07 19:41:53 +0200

    Extract out functions.

--- src/extension.ts
+++ src/extension.ts
@@ -5,109 +5,124 @@ type DocumentLine = {
 	lineNumber: number
 };
 
+function getDocumentLines(document: vscode.TextDocument): DocumentLine[] {
+
+	return [...Array(document.lineCount).keys()].map(lineNumber => {
+
+		return {
+			text: document.lineAt(lineNumber).text,
+			lineNumber: lineNumber,
+		};
+	});
+}
+
+function insertNewLineAction(lineNumber: number, position: number): vscode.TextEdit {
+	return vscode.TextEdit.insert(new vscode.Position(lineNumber, position), '\n');
+}
+
+function insertNewTabAction(lineNumber: number, position: number): vscode.TextEdit {
+	return vscode.TextEdit.insert(new vscode.Position(lineNumber, position), '\t');
+}
+
+function getTextEditActions(documentLines: DocumentLine[]): vscode.TextEdit[] {
+
+	function getTextEditActions(documentLine: DocumentLine): vscode.TextEdit[] {
+
+		const text = documentLine.text;
+
+		const characters = text.split('');
+
+		const characterPositionsNotInAString = (char: String) => {
+
+			const stringPositionIndices = characters
+				.flatMap((char, index) => {
+					return char === '"' ? [index] : [];
+				})
+				.reduce<[[number, number][], number | null]>(([stringPositionIndices, buffer], quotationMarkPosition) => {
+
+					return !!buffer
+						? [[...stringPositionIndices, [buffer, quotationMarkPosition]], null]
+						: [stringPositionIndices, quotationMarkPosition];
+				}, [[], null])[0];
+
+			const characterPositionContainedInAString = (index: number) => {
+				return stringPositionIndices.some(([start, end]) =>
+					start <= index && index < end
+				);
+			};
+
+			return characters.flatMap((c, index) =>
+				c === char && !characterPositionContainedInAString(index)
+					? [index]
+					: []
+			);
+		};
+
+		const openingBracketPositionsNotInAString = characterPositionsNotInAString('(');
+
+		const firstOpeningBracketNotInString = openingBracketPositionsNotInAString[0];
+
+		var editActions: vscode.TextEdit[] = [];
+
+		if (!!firstOpeningBracketNotInString) {
+
+			const insertNewLineAfterBracket = insertNewLineAction(documentLine.lineNumber, firstOpeningBracketNotInString + 1);
+			const insertTabInNewLine = insertNewTabAction(documentLine.lineNumber, firstOpeningBracketNotInString + 1);
+
+			editActions = [...editActions, insertNewLineAfterBracket, insertTabInNewLine];
+		}
+
+		const didNotMatchString = "did not contain the same elements as ";
+		const didNotMatchStringStartIndex = text.indexOf(didNotMatchString);
+
+		const firstOpeningBracketAfterDidNotMatch = openingBracketPositionsNotInAString
+			.find(index => index > didNotMatchStringStartIndex);
+
+		if (!!firstOpeningBracketAfterDidNotMatch) {
+
+			const insertNewLineAfterBracket = insertNewLineAction(documentLine.lineNumber, firstOpeningBracketAfterDidNotMatch + 1);
+			const insertTabInNewLine = insertNewTabAction(documentLine.lineNumber, firstOpeningBracketAfterDidNotMatch + 1);
+
+			editActions = [...editActions, insertNewLineAfterBracket, insertTabInNewLine];
+		}
+
+		const spacePositionsNotInAString = characterPositionsNotInAString(' ');
+
+		spacePositionsNotInAString
+			.filter(index =>
+				(index < didNotMatchStringStartIndex) || (index > didNotMatchStringStartIndex + didNotMatchString.length)
+			)
+			.forEach(index => {
+
+				const insertNewLine = insertNewLineAction(documentLine.lineNumber, index + 1);
+				const insertTabInNewLine = insertNewTabAction(documentLine.lineNumber, index + 1);
+
+				editActions = [...editActions, insertNewLine, insertTabInNewLine];
+			});
+
+		if (didNotMatchStringStartIndex >= 0) {
+
+			const insertTwoNewLinesBefore = vscode.TextEdit.insert(new vscode.Position(documentLine.lineNumber, didNotMatchStringStartIndex), '\n\n');
+			const insertTwoNewLinesAfter = vscode.TextEdit.insert(new vscode.Position(documentLine.lineNumber, didNotMatchStringStartIndex + didNotMatchString.length), '\n\n');
+
+			editActions = [...editActions, insertTwoNewLinesBefore, insertTwoNewLinesAfter];
+		}
+
+		return editActions;
+	}
+
+	return documentLines.flatMap(documentLine => getTextEditActions(documentLine));
+}
+
 export function activate() {
 
 	vscode.languages.registerDocumentFormattingEditProvider('scala-collections', {
 
 		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
 
-			const documentLines: DocumentLine[] = [...Array(document.lineCount).keys()].map(lineNumber => {
+			const documentLines: DocumentLine[] = getDocumentLines(document);
 
-				return {
-					text: document.lineAt(lineNumber).text,
-					lineNumber: lineNumber,
-				};
-			});
-
-			const textEditActions = documentLines.reduce<vscode.TextEdit[]>((editActions, documentLine) => {
-
-				const lineText = documentLine.text;
-
-				const lineCharacters = lineText.split('');
-
-				const stringBorders: [number, number][] = lineCharacters
-					.reduce<number[]>((borders, char, index) => {
-						if (char === '"') {
-							return [...borders, index];
-						} else {
-							return borders;
-						};
-					}, [])
-					.reduce<[[number, number][], number | null]>(([borders, buffer], cur) => {
-						if (!!buffer) {
-							return [[...borders, [buffer, cur]], null];
-						} else {
-							return [borders, cur];
-						}
-					}, [[], null])[0];
-
-				const containedInString = (index: number) => {
-					return stringBorders.some(([start, end]) => {
-						return start <= index && index < end;
-					});
-				};
-
-				const findFirstOccurrenceNotInString = (text: String) => lineCharacters.map((char, index) => {
-					if (char === text && !containedInString(index)) {
-						return [index];
-					} else {
-						return [];
-					}
-				}).reduce((acc, val) => acc.concat(val), []);
-
-				const openingBracketsNotInString = findFirstOccurrenceNotInString('(');
-
-				const spacesNotInString = findFirstOccurrenceNotInString(' ');
-
-				const didNotMatchString = "did not contain the same elements as ";
-
-				const didNotMatchStringIndex = lineText.indexOf(didNotMatchString);
-
-				const firstOpeningBracketNotInString = openingBracketsNotInString[0];
-
-				const firstOpeningBracketAfterDidNotMatch = openingBracketsNotInString
-					.filter(index => (index > didNotMatchStringIndex))[0];
-
-				var actions: vscode.TextEdit[] = [];
-
-				if (firstOpeningBracketNotInString >= 0) {
-
-					const insertNewLineAfterBracket = vscode.TextEdit.insert(new vscode.Position(documentLine.lineNumber, firstOpeningBracketNotInString + 1), '\n');
-					const insertTabInNewLine = vscode.TextEdit.insert(new vscode.Position(documentLine.lineNumber, firstOpeningBracketNotInString + 1), '\t');
-
-					actions = [...actions, insertNewLineAfterBracket, insertTabInNewLine];
-				}
-
-				if (firstOpeningBracketAfterDidNotMatch >= 0) {
-
-					const insertNewLineAfterBracket = vscode.TextEdit.insert(new vscode.Position(documentLine.lineNumber, firstOpeningBracketAfterDidNotMatch + 1), '\n');
-					const insertTabInNewLine = vscode.TextEdit.insert(new vscode.Position(documentLine.lineNumber, firstOpeningBracketAfterDidNotMatch + 1), '\t');
-
-					actions = [...actions, insertNewLineAfterBracket, insertTabInNewLine];
-				}
-
-				spacesNotInString
-					.filter(index => {
-						return index < didNotMatchStringIndex || index > didNotMatchStringIndex + didNotMatchString.length;
-					})
-					.forEach(index => {
-
-						const insertNewLine = vscode.TextEdit.insert(new vscode.Position(documentLine.lineNumber, index + 1), '\n');
-						const insertTabInNewLine = vscode.TextEdit.insert(new vscode.Position(documentLine.lineNumber, index + 1), '\t');
-
-						actions = [...actions, insertNewLine, insertTabInNewLine];
-					});
-
-				if (didNotMatchStringIndex >= 0) {
-
-					const insertTwoNewLinesBefore = vscode.TextEdit.insert(new vscode.Position(documentLine.lineNumber, didNotMatchStringIndex), '\n\n');
-					const insertTwoNewLinesAfter = vscode.TextEdit.insert(new vscode.Position(documentLine.lineNumber, didNotMatchStringIndex + didNotMatchString.length), '\n\n');
-
-					actions = [...actions, insertTwoNewLinesBefore, insertTwoNewLinesAfter];
-				}
-
-				return [...editActions, ...actions];
-			}, []);
+			const textEditActions = getTextEditActions(documentLines);
 
 			return textEditActions;
 		}
